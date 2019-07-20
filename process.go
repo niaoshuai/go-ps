191339daa78e7812e8f40aaa36ef245c5c587b5e
niaoshuai <niao.shuai123@163.com>
2019-07-20 14:23:46 +0800

    Update

--- process.go
+++ process.go
@@ -19,6 +19,8 @@ type Process interface {
 	// Executable name running this process. This is not a path to the
 	// executable.
 	Executable() string
+
+	Env() []string
 }
 
 // Processes returns all processes.
